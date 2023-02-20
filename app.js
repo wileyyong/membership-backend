e9b95a5d2c48cadba9b601a77461e8ab8081fdf1
Dev <dev@Devs-MacBook-Pro.local>
2023-02-20 14:14:34 -0600

    update cors

--- app.js
+++ app.js
@@ -113,16 +113,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
-app.use((req, res, next) => {
-    //allow access to current url. work for https as well
-    res.setHeader('Access-Control-Allow-Origin',req.header('Origin'));
-    res.removeHeader('x-powered-by');
-    //allow access to current method
-    res.setHeader('Access-Control-Allow-Methods',req.method);
-    res.setHeader('Access-Control-Allow-Headers','Content-Type');
-    next();
-  });
+app.use(cors({origin:'*'}));
 app.use('/', login);
 app.use('/booking', bookingRoute);
 app.use('/register', registerRouter);  // To register page 
