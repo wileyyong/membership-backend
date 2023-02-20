fb440071264a3077725665eca4f05833d100b2a2
Dev <dev@Devs-MacBook-Pro.local>
2023-02-20 15:58:16 -0600

    vercel deploy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // MongoURI: "mongodb+srv://nestapp:jcVppDPO4n0fvHnU@nestmain.jffxv.mongodb.net/lets_build?retryWrites=true&w=majority"
+    MongoURI: "mongodb+srv://nestapp:jcVppDPO4n0fvHnU@nestmain.jffxv.mongodb.net/lets_build?retryWrites=true&w=majority"
     // MongoURI: "mongodb://127.0.0.1:27017/db_letsbuild"
-MongoURI:"mongodb+srv://prakashVaithianathan:Indu07Praka18@cluster0.mruhe.mongodb.net/demoWiley?retryWrites=true&w=majority"
+// MongoURI:"mongodb+srv://prakashVaithianathan:Indu07Praka18@cluster0.mruhe.mongodb.net/demoWiley?retryWrites=true&w=majority"
 }
